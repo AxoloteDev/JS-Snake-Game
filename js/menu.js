22b2d8e014adb3bb53b42d4848c850a444c81d1e
AxoloteDev <axolote.dev@gmail.com>
2023-11-07 17:47:53 -0300

    Inseri um menu navegável que é uma ul junto de três li onde cada opção representa uma ação possível. Também foi criado um arquivo JS que controla a lógica de navegação do menu, o arquivo é dividido em controle por teclado e controle por mouse. Esse código usa de uma variável numérica que funciona como índex junto de uma classe para controlar em qual opção do menu estamos. No CSS foi feito o estilo básico do menu, centralizando os itens na tela, permitindo melhor visualização da navegação.

---
+++ js/menu.js
@@ -0,0 +1,71 @@
+const menu = document.querySelector("ul");
+let itens = menu.querySelectorAll("li");
+let index = 0;
+
+//? Menu keyboard navegation
+document.addEventListener("keydown", (event)=>{
+    if(event.key === "ArrowDown" || event.key === "s"){
+        index++;
+
+        if(index >= itens.length){
+            itens[2].classList.remove("select");
+            index = 0;
+        }
+
+        itens[index].classList.add("select"); 
+
+        if(itens[index - 1]){
+            itens[index - 1].classList.remove("select");
+        }
+    }
+
+    if(event.key === "ArrowUp" || event.key === "w"){
+        index--;
+
+        if(index < 0){
+            itens[0].classList.remove("select");
+            index = 2;
+        }
+
+        itens[index].classList.add("select"); 
+
+        if(itens[index + 1]){
+            itens[index + 1].classList.remove("select");
+        }
+    }
+
+    if(event.key === "Enter"){
+        if(itens[0].classList == "select"){
+            console.log("opção 1");
+        }
+        if(itens[1].classList == "select"){
+            console.log("opção 2");
+        }
+        if(itens[2].classList == "select"){
+            console.log("opção 3");
+        }
+    }
+});
+
+//? Menu mouse navegation
+itens.forEach((iten)=>{
+    iten.addEventListener("mouseover", ()=>{
+        itens.forEach((iten)=> {
+            iten.classList.remove("select");
+        })
+
+        iten.classList.add("select");
+
+        iten.addEventListener("click", () => {
+            if(itens[0].classList == "select"){
+                console.log("opção 1");
+            }
+            if(itens[1].classList == "select"){
+                console.log("opção 2");
+            }
+            if(itens[2].classList == "select"){
+                console.log("opção 3");
+            }
+        })
+    })
+});
